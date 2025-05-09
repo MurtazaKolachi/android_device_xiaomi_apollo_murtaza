8eb0b54911738aa616851cd0279e07dc3f97a96d
Murtaza Hussain <murtazakolachi7@gmail.com>
2025-05-09 22:52:17 +0500

    apollo: Organise DT

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
@@ -120,10 +124,6 @@ PRODUCT_PACKAGES += \
 # Consumer IR
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.xiaomi
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
@@ -165,11 +165,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     IFAAService
 
-# IPACM
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
-
 # Input
 PRODUCT_PACKAGES += \
     Xiaomi_Smart_Pen_Keyboard.kl \
@@ -183,6 +178,11 @@ PRODUCT_PACKAGES += \
     uinput-fortsense.idc \
     uinput-fpc.idc \
     uinput-goodix.idc
+
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml
 
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
