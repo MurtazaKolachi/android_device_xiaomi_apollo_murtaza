4da9826d22474b87dd15149ea20fbf23150a13fa
Murtaza Hussain <murtazakolachi7@gmail.com>
2025-05-20 18:53:40 +0500

    apollo: build with own kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,10 +68,8 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_SOURCE := kernel/xiaomi/apollo
 TARGET_KERNEL_CONFIG := \
-    vendor/kona-perf_defconfig \
-    vendor/debugfs.config \
-    vendor/xiaomi/sm8250-common.config \
-    vendor/xiaomi/apollo.config
+    apollo_defconfig \
+    vendor/debugfs.config
 
 # Media
 TARGET_USES_ION := true
